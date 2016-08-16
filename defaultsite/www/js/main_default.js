a269e6eaf4d2ab6e291263bf16ff32e8a2a82f26
b2blosangeles <b2blosangeles@gmail.com>
2016-08-15 22:59:40 -0700

    test

--- defaultsite/www/js/main_default.js
+++ defaultsite/www/js/main_default.js
@@ -113,7 +113,7 @@ app.controller('microserviceReportController', function($rootScope, $scope, $loc
 		$scope.$parent.progress('on', 'post form');
 		$http({
 		  method: 'POST',
-		  url: '/_git/removeForm',
+		  url: '/_git/removeVhost',
 		  data: v
 		}).then(function successCallback(response) {
 			alert('removed');
