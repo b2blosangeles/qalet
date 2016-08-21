35b4b31992c03da0f2e0f8ab11fe25208efcd484
b2blosangeles <b2blosangeles@gmail.com>
2016-08-21 16:24:07 -0700

    test

--- modules/testModule/testModule.js
+++ modules/testModule/testModule.js
@@ -1,5 +1,5 @@
 var path = require('path');
-require( path.join(__dirname, '../../', 'package/mocha/modules/mocha') );
+require( path.join(__dirname, '../../', 'package/mocha/node_modules/mocha') );
 console.log('aaa');
 /*
 var assert = require('assert');
