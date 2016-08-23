ea0a14c6cb79a71d52a2baf1ce10014a4abd3012
b2blosangeles <b2blosangeles@gmail.com>
2016-08-23 16:18:13 -0700

    test

--- cron/cron_service/git_service.js
+++ cron/cron_service/git_service.js
@@ -20,36 +20,42 @@ var pkg = {
 				
 };
 
-pkg.db.vhost.find({}).sort({ created: -1 }).exec(function (err, docs) {
+pkg.db.vhost.find({}).sort({ created: -1 }).exec(function (err, vhost) {
 	var CP = new pkg.crowdProcess();
 	
-	
-	exec('cd ' + __dirname + ' && git pull', function(error, stdout, stderr) {
-		console.log({stdout:stdout, stderr:stderr})
-	});	
+	var _f = {};			
+			
+	_f['D0'] = function(cbk) {
+		
+		exec('cd ' + env.root_space + ' && git pull', function(error, stdout, stderr) {
+			cbk({stdout:stdout, stderr:stderr});
+		});						
+	}	
 	
 	if (!err) {
-		for (var i=0; i < docs.length; i++) {
-			var _f = function(v) {
-				return function() {
-					pkg.fs.exists(env.root_space + '_microservice/' + v['name'], function(exists) {
-						if (exists) {
-							exec('cd ' + env.root_space + '_microservice/' + v['name'] + '&& git pull', function(err, out, code) {
-								console.log(v['name'] + '::' + out);
-							});
-						} else {
-							console.log(v['name'] + 'SKIPPED');
-						}
-					});	
-				}
-					
-				
-			};
-		
-			_f(docs[i])();
+		for (var i=0; i < vhost.length; i++) {
+			
+			_f['C'+i] = function(cbk) {
+				pkg.fs.exists(env.root_space + '_microservice/' + vhost[i]['name'], function(exists) {
+					if (exists) {
+						exec('cd ' + env.root_space + '_microservice/' + vhost[i]['name'] + '&& git pull', function(err, out, code) {
+							cbk(vhost[i]['name'] + '::' + out);
+						});
+					} else {
+						cbk(vhost[i]['name'] + 'SKIPPED');
+					}
+				});					
+			}				
 		}
-	} else {
-		console.log(err)
-	}
+	} 
+	CP.serial(
+		_f,
+		function(data) {
+			console.log(data);
+		},
+		300000
+	);	
+	
+	
 });
 
