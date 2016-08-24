5419455a267cc39c4d25afbfcd77722068ab756c
b2blosangeles <b2blosangeles@gmail.com>
2016-08-24 08:23:12 -0700

    test

--- cron/cron_service/git_service.js
+++ cron/cron_service/git_service.js
@@ -54,8 +54,8 @@ pkg.db.vhost.find({}).sort({ created: -1 }).exec(function (err, vhost) {
 		function(data) {
 			var tm = new Date();
 			// { $lt : new Date(tm.getTime() - 60000) }
-			pkg.db.git_log.remove({time: { $lt : new Date(tm.getTime() - 120000) }}, { multi: true }, function (err) {
-				pkg.db.git_log.insert({time: new Date().getTime(),  data:data}, function (err) {
+			pkg.db.git_log.remove({}, { multi: true }, function (err) {
+				pkg.db.git_log.insert({time: new Date(),  data:data}, function (err) {
 					if (err) console.log(err);
 					else console.log(tm.getTime());
 				});
